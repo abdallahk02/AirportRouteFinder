199d7526bb4b96158eb3b9f79d2dcc81f9bb8520
abdallahk02 <155988226+abdallahk02@users.noreply.github.com>
2026-03-27 17:08:29 -0700

    Add minDist function declaration
    Added minDist function declaration to Algorithm.h

--- include/Algorithm.h
+++ include/Algorithm.h
@@ -3,16 +3,16 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include <string>
 #include "Airport.h"
-
-
-
 
 // Returns distance in kilometers
 double calculateHaversine(double lat1, double lon1, double lat2, double lon2);
 
 std::vector<std::string> minStops(int srcID, int desID, std::unordered_map<int, Airport> airports);
+
+std::vector<std::string> minDist(int srcID, int desID, std::unordered_map<int, Airport> airports);
 
 #endif
